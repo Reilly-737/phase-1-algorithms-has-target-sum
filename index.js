a75740c5db599f1f6d32eed463921c7cd2fec2e7
+ <+>
2023-08-11 09:53:19 -0600

    finish lab

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let numSet = new Set();
+  for(let i = 0; i < array.length; i++){ 
+   if(numSet.has(target - array[i])){
+    return true;
+   }
+   numSet.add(array[i]);
+ }
+ return false; 
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
